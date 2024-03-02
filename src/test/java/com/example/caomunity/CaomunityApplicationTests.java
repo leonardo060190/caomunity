4cdf0109cc654ab48d1cfc4c350ef13fd3cfb087
leonardo060190 <leonardo-morruga@hotmail.com>
2024-03-02 17:59:57 -0300

    inicio do projeto caomunity

---
+++ src/test/java/com/example/caomunity/CaomunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caomunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaomunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
